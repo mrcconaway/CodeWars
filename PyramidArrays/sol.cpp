3709e806d072cc92e18704b57b46b3b926431e08
mrcconaway <mrcconaway@gmail.com>
2022-05-18 14:25:10 -0400

    passed

--- PyramidArrays/sol.cpp
+++ PyramidArrays/sol.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vectors>
+#include <vector>
 #include <string>
 #include <cstdlib>
 
@@ -9,7 +9,7 @@ std::vector<std::vector<int>> pyramid(std::size_t n);
 int main()
 {
     std::size_t n = 2;
-    pyramid(2);
+    std::vector<std::vector<int>> fin = pyramid(2);
 
     return 0;
 }
@@ -17,18 +17,9 @@ int main()
 
 std::vector<std::vector<int>> pyramid(std::size_t n) {
     std::vector<std::vector<int>> ans;
-    for(int i = 0; i <= n; i++ ){
-        std::vector<int> sub;
-        ans.push_back(sub(1,n));
+    for(int i = 1; i <= n; i++ ){
+        std::vector<int> sub(i,1);
+        ans.push_back(sub);
     }
-    //assuming you have a "2D" vector vvi (vector of vector of int's)
-    vector< vector<int> >::iterator row;
-    vector<int>::iterator col;
-    for (row = ans.begin(); row != ans.end(); row++) {
-        for (col = row->begin(); col != row->end(); col++) {
-            // do stuff ...
-            std::cout << *col << std::endl;
-        }
-    }
-
+  return ans;
 }
