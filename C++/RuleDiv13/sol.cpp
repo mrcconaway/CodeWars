ee5873fd3087bd2ca882e96b8cb16ada3bf3a859
Ryan Conaway <mrcconaway@gmail.com>
2022-05-27 15:19:30 -0400

    Completed Kata but still needs to be submitted on codewars

--- C++/RuleDiv13/sol.cpp
+++ C++/RuleDiv13/sol.cpp
@@ -10,9 +10,17 @@ public:
 	Thirteen(){
 		ans = thirt(1234567);
 	}
-	long long ans;
-	void parserFunc(vector<int>& vinp, int n){
 
+	long long ans;
+
+	static vector<int> parserFunc(long long ln){
+		vector<int> tmp;
+		std::string parser = std::to_string(ln);
+		int coversionFactor = 48;
+		for(auto i = 0; i < parser.size(); ++i){
+			tmp.push_back(parser[i] - coversionFactor);
+		}
+		return tmp;
 	}
 
 public:
@@ -22,25 +30,23 @@ public:
 long long Thirteen::thirt(long long n)
 {
 	long long stat = 0;
-	std::string parser = std::to_string(n);
 	vector<int> vint;
-
-	int coversionFactor = 48;
 	vector<int> modulo = {1 , 10, 9, 12, 3, 4}; // powers of 10 mod 13
-
-	for(auto i = 0; i < parser.size(); ++i){
-		vint.push_back(parser[i] - coversionFactor);
-	}
-
 	long long tmp; 
+	vint = parserFunc(n);
 	do{
 		// need to loop until get a repeated value
 		stat = tmp;
+		tmp = 0;
 		// cout << vint.size() << " " << parser.size() << " " << modulo.size() << endl;
 		for(int i=0; i < vint.size(); i++){
-			tmp += vint[i] * modulo[ i % modulo.size()];
-
+			tmp += vint[vint.size()-i-1] * modulo[ i % modulo.size()];
+			cout << i << " " << tmp << " " << vint[i] << " " << modulo[ i % modulo.size()] << endl;
+			std::string trsh;
+			std::getline(cin, trsh);
 		}
+		vint = parserFunc(tmp);
+		cout << tmp << endl;
 
 	}while(stat != tmp );
 
