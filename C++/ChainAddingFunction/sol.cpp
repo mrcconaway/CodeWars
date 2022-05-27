200be37594c40dbfaf365c804b509c4dcc018dc2
Ryan Conaway <mrcconaway@gmail.com>
2022-05-27 16:20:10 -0400

    Can't figure this one out

--- C++/ChainAddingFunction/sol.cpp
+++ C++/ChainAddingFunction/sol.cpp
@@ -2,13 +2,45 @@
 
 using namespace std;
 
-void tmp(){
 
-}
 
+// #include <iostream>
+#include <string>
+
+// struct MyClass {
+//   std::string value = "MyClass";
+
+//   MyClass operator+(int x) {
+//     return {value + " + " + std::to_string(x)};
+//   }
+
+//   operator std::string() {
+//     return value;
+//   }
+// };
+
+// int main() {
+//   MyClass myObject;
+
+//   std::cout << std::string(myObject + 1 + 2 + 3) << '\n';
+// }
+
+
+struct sol{
+	int value = 0;
+	sol operator+(int x){
+		return {value + x};
+	}
+
+
+
+};
+	auto add(int n){
+		return (value+n); 
+	}
 int main()
 {
-	void = tmp();
-
+	sol test;
+	sol::add(5);
 	return 0;
 }
